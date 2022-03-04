e22942d5dbbf1e3a05527fd60e3c79d145279787
Ashley Cho <ashleycho.slp@gmail.com>
2022-03-04 00:32:07 -0800

    Add queries and mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+// Query for logged in users
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+  
+`;
